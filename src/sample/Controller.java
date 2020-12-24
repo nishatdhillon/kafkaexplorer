51f77fb558409a9656e8c70f0378c01a27d0cde6
stephaneuh <steph@DESKTOP-15P9GKB.localdomain>
2020-12-24 12:40:54 -0500

    initial commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+import javax.swing.plaf.basic.BasicTreeUI;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+@FXML
+private TreeView<String> kafkaTree;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        TreeItem<String> root = new TreeItem<>("Kafka Clusters");
+        //build kafka cluster tree
+        TreeItem cluster = new TreeItem("Cluster01");
+        root.getChildren().add(cluster);
+        kafkaTree.setRoot(root);
+        root.setExpanded(true);
+
+    }
 }
