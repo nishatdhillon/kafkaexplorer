8357c671c93a59fbb727d1c1c7e102be00e01436
stephaneuh <steph@DESKTOP-15P9GKB.localdomain>
2021-01-03 10:45:40 -0500

    Adding error mgmt when loading config.yaml

--- src/main/java/com/kafkaexplorer/Main.java
+++ src/main/java/com/kafkaexplorer/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/kafkaExplorer.fxml"));
         primaryStage.setTitle("Kafka Explorer");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/ke-logo-font-15.png")));
         primaryStage.setScene(new Scene(root, 1280, 600));
         primaryStage.show();
     }
