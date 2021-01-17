aea867fea69a6f4573d7fd07fb640a362e729c43
stephaneuh <stephane.ris@bnc.ca>
2021-01-16 20:50:31 -0500

    build mac-os app

--- appify/create-macos-app.sh
+++ appify/create-macos-app.sh
@@ -4,7 +4,12 @@ rm -rf kafkaexplorer-1.1.app
 ./appify.sh run.command kafkaexplorer-1.1
 mkdir kafkaexplorer-1.1.app/Contents/MacOS/releases
 cp ../releases/kafkaexplorer-1.1-osx.jar kafkaexplorer-1.1.app/Contents/MacOS/releases
-rm -rf ../releases/kafkaexplorer-1.1.app
-mv kafkaexplorer-1.1.app ../releases
+rm -rf kafkaexplorerDMG
+mkdir kafkaexplorerDMG
+mv kafkaexplorer-1.1.app kafkaexplorerDMG
+hdiutil create /tmp/tmp.dmg -ov -volname "kafkaexplorer-1.1" -fs HFS+ -srcfolder "kafkaexplorerDMG"
+hdiutil convert /tmp/tmp.dmg -format UDZO -o kafkaexplorer-1.1.dmg
+rm -rf ../releases/kafkaexplorer-1.1.dmg
+mv kafkaexplorer-1.1.dmg ../releases
 
 
