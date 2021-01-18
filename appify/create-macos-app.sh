7fd894988a44718a71de291bd368149c45f5f36d
stephaneuh <stephane.ris@icloud.com>
2021-01-17 20:53:53 -0500

    add app icon

--- appify/create-macos-app.sh
+++ appify/create-macos-app.sh
@@ -3,9 +3,9 @@ cd -- "$(dirname -- "$BASH_SOURCE")"
 rm -rf kafkaexplorer-1.1.app
 ./appify.sh run.command kafkaexplorer-1.1
 mkdir kafkaexplorer-1.1.app/Contents/MacOS/releases
-mkdir kafkaexplorer-1.1.app/Ressources
+mkdir kafkaexplorer-1.1.app/Resources
 cp Info.plist kafkaexplorer-1.1.app/Contents
-cp ../img/ke-icon-blue.icns kafkaexplorer-1.1.app/Ressources
+cp ../img/ke-icon-blue.icns kafkaexplorer-1.1.app/Resources
 cp ../releases/kafkaexplorer-1.1-osx.jar kafkaexplorer-1.1.app/Contents/MacOS/releases
 rm -rf kafkaexplorerDMG
 mkdir kafkaexplorerDMG
