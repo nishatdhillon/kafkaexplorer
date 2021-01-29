c1bd149e94f413668afceab7e3ac44bd94cee345
stephaneuh <stephane.ris@icloud.com>
2021-01-29 13:15:18 -0500

    add custom css

--- src/main/java/com/kafkaexplorer/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.kafkaexplorer;
-
-public class Launcher {
-
-    public static void main(String[] args) {
-        Main.main(args);
-    }
-}
--- src/main/java/com/kafkaexplorer/Main.java
+++ src/main/java/com/kafkaexplorer/Main.java
@@ -1,22 +1,31 @@
 package com.kafkaexplorer;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import java.util.Locale;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+
         Locale.setDefault(new Locale("en", "CA"));
         Parent root = FXMLLoader.load(getClass().getResource("/kafkaExplorer.fxml"));
         primaryStage.setTitle("Kafka Explorer (community-edition)");
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/ke-logo-font-15.png")));
-        primaryStage.setScene(new Scene(root, 1280, 600));
+        Scene scene = new Scene(root, 1280, 600);
+        scene.getStylesheets().add(getClass().getResource("/main.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
